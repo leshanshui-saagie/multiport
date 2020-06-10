83a789741e9a15f7396e71496822a0b99ebaa949
leshanshui-saagie <65134274+leshanshui-saagie@users.noreply.github.com>
2020-06-10 14:22:18 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
 application = Flask(__name__)
  
-@application.route("/")
+@application.route("/", methods=['POST'])
 def hello():
     return jsonify({'response': "Hello World!"}), 201
- 
+
+@application.route("/raw", methods=['POST'])
+def raw():
+    return jsonify({'response': "Raw Method"}), 201
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=8079)
